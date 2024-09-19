e3bf0e8ad63ddeb490eef5581ef1cccc931b266c
Hafiz Ibrahim <hafizzibrahimm0@gmail.com>
2024-09-19 13:21:28 +0700

    add main page (Bottom NavBar)

---
+++ lib/app/modules/main/controllers/main_controller.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
+import 'package:maritimmuda_connect/app/modules/home/views/home_view.dart';
+import 'package:maritimmuda_connect/app/modules/login/views/login_view.dart';
+
+class MainController extends GetxController with GetTickerProviderStateMixin {
+  var bottomNavIndex = 0.obs;
+
+  late AnimationController fabAnimationController;
+  late AnimationController borderRadiusAnimationController;
+  late Animation<double> fabAnimation;
+  late Animation<double> borderRadiusAnimation;
+  late CurvedAnimation fabCurve;
+  late CurvedAnimation borderRadiusCurve;
+  late AnimationController hideBottomBarAnimationController;
+
+  final iconList = <IconData>[
+    Icons.home,
+    Icons.show_chart_rounded,
+    Icons.shopping_bag,
+    Icons.person,
+  ];
+
+  final List<String> titleList = [
+    "Home",
+    "Analytics",
+    "Catalog",
+    "Profile",
+  ];
+
+  final List<Widget> viewList = [
+    HomeView(),
+    HomeView(),
+    HomeView(),
+    LoginView(),
+  ];
+
+  @override
+  void onInit() {
+    super.onInit();
+    fabAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    borderRadiusAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+    fabCurve = CurvedAnimation(
+      parent: fabAnimationController,
+      curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+    borderRadiusCurve = CurvedAnimation(
+      parent: borderRadiusAnimationController,
+      curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+
+    fabAnimation = Tween<double>(begin: 0, end: 1).animate(fabCurve);
+    borderRadiusAnimation = Tween<double>(begin: 0, end: 1).animate(
+      borderRadiusCurve,
+    );
+
+    hideBottomBarAnimationController = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    Future.delayed(
+      const Duration(seconds: 1),
+      () => fabAnimationController.forward(),
+    );
+    Future.delayed(
+      const Duration(seconds: 1),
+      () => borderRadiusAnimationController.forward(),
+    );
+  }
+
+  bool onScrollNotification(ScrollNotification notification) {
+    if (notification is UserScrollNotification &&
+        notification.metrics.axis == Axis.vertical) {
+      switch (notification.direction) {
+        case ScrollDirection.forward:
+          hideBottomBarAnimationController.reverse();
+          fabAnimationController.forward(from: 0);
+          break;
+        case ScrollDirection.reverse:
+          hideBottomBarAnimationController.forward();
+          fabAnimationController.reverse(from: 1);
+          break;
+        case ScrollDirection.idle:
+          break;
+      }
+    }
+    return false;
+  }
+
+  void onBottomNavTap(int index) {
+    bottomNavIndex.value = index;
+  }
+}
---
+++ lib/app/modules/main/views/main_view.dart
@@ -0,0 +1,85 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'package:get/get.dart';
+import 'package:maritimmuda_connect/themes.dart';
+
+import '../controllers/main_controller.dart';
+
+class MainView extends GetView<MainController> {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      extendBody: true,
+      body: Obx(
+        // Menampilkan halaman berdasarkan bottomNavIndex
+        () => controller.viewList[controller.bottomNavIndex.value],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryDarkBlueColor,
+        shape: CircleBorder(),
+        child: Icon(
+          Icons.credit_card,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          controller.fabAnimationController.reset();
+          controller.borderRadiusAnimationController.reset();
+          controller.borderRadiusAnimationController.forward();
+          controller.fabAnimationController.forward();
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: Obx(
+        () => AnimatedBottomNavigationBar.builder(
+          itemCount: controller.iconList.length,
+          tabBuilder: (int index, bool isActive) {
+            final color = isActive ? colors.primary : Colors.grey;
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  controller.iconList[index],
+                  size: 24,
+                  color: primaryDarkBlueColor,
+                ),
+                const SizedBox(height: 4),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Text(
+                    controller.titleList[index],
+                    maxLines: 1,
+                    style: TextStyle(color: color),
+                  ),
+                )
+              ],
+            );
+          },
+          backgroundColor: Colors.white,
+          activeIndex: controller.bottomNavIndex.value,
+          splashColor: colors.primary,
+          notchAndCornersAnimation: controller.borderRadiusAnimation,
+          splashSpeedInMilliseconds: 300,
+          notchSmoothness: NotchSmoothness.defaultEdge,
+          gapLocation: GapLocation.center,
+          leftCornerRadius: 16,
+          rightCornerRadius: 16,
+          onTap: controller.onBottomNavTap,
+          hideAnimationController: controller.hideBottomBarAnimationController,
+          shadow: BoxShadow(
+            offset: const Offset(0, 1),
+            blurRadius: 12,
+            spreadRadius: 0.5,
+            color: colors.primary,
+          ),
+        ),
+      ),
+    );
+  }
+}
